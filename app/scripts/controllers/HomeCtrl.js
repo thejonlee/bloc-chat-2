07989087b3081040fc8117f9042894ec5bcaca1a
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-08-24 23:30:11 -0700

    CP 4 List Messages completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,21 +1,26 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
         // Assign Room array retrieved by all method to $scope variable
-        var room = this;
+        this.rooms = Room.all;
+        this.currentRoom = null;
+        this.messages = null;
 
-        room.rooms = Room.all;
-
-        room.newRoom = function () {
+        this.newRoom = function () {
             $uibModal.open({
               templateUrl: '/templates/modal.html',
               size: 'sm',
               controller: 'ModalCtrl as modal'
             });
-        }
+        };
+
+        this.setCurrentRoom = function (room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(room.$id);
+        };
 
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
